864536b5619782ffc6a81ffa23e65b8318905777
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-04-02 18:20:21 +0300

    Great Alert!

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
@@ -16,28 +16,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       guard let secondVC = segue.destination as? SecondViewController else { return }
-       secondVC.userName = userNameTF.text
-    }
-
-    @IBAction func logInButtonTapped(_ sender: UIButton) {
-        if userNameTF.text == "1" {
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if userNameTF.text == "Kirill" && passwordTF.text == "12345" {
+            guard let secondVC = segue.destination as? SecondViewController else { return }
+            secondVC.userName = userNameTF.text
+        } else {
+            showAlert(
+                withTitle: "Alert!",
+                andMessage: "Error User Name or Password"
+            )
         }
-
     }
-
 
     @IBAction func forgotNameButtonTapped(_ sender: UIButton) {
-
+        showAlert(
+            withTitle: "Forgot User Name",
+            andMessage: "You User Name is Kirill"
+        )
     }
 
     @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
-
+        showAlert(
+            withTitle: "Forgot password",
+            andMessage: "You password 12345"
+        )
     }
-    
+
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 
 }
 
