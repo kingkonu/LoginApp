6abc5d62c2f1bed9a0186dac5a796e9484b3f8b2
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-04-02 17:44:01 +0300

    Create view

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       guard let secondVC = segue.destination as? SecondViewController else { return }
+       secondVC.userName = userNameTF.text
+    }
+
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+        if userNameTF.text == "1" {
+
+
+        }
+
+    }
+
+
+    @IBAction func forgotNameButtonTapped(_ sender: UIButton) {
+
+    }
+
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+
+    }
+    
 
 }
 
