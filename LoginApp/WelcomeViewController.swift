95daae49f42c9efeb03beff9f83eb380fc492bd4
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-04-05 19:24:25 +0300

    Change GradientColor

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -10,17 +10,41 @@ import UIKit
 final class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var welcomeLabel: UILabel!
-    @IBOutlet weak var handLabel: UILabel!
 
-    var userName: String!
+    var userName = ""
+
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeLabel.text = userName
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [UIColor.systemCyan.cgColor, UIColor.systemFill.cgColor]
-        view.layer.insertSublayer(gradientLayer, at: 0)
+        view.addVerticalGradientLayer(
+            topColor: primaryColor,
+            bottomColor: secondaryColor
+        )
+        welcomeLabel.text = "Welcome, \(userName)!"
     }
+}
 
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
 }
