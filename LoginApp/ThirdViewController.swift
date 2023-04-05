567bbba0d951dec14986769c4ec9a04c6048cd4d
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-04-05 20:50:29 +0300

    Logic TabBar 2

---
+++ LoginApp/ThirdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.swift
+//  LoginApp
+//
+//  Created by Alexey Kanaev on 4/5/23.
+//
+
+import UIKit
+
+final class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
