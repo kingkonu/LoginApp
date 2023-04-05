98fede964ef4e2357c31ffd3c8454f6f31fcd126
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-04-05 22:41:59 +0300

    Model 8

---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Alexey Kanaev on 4/5/23.
+//
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+
+    static func getUser() -> User {
+        User(
+            login: "Rock",
+            password: "Climbing",
+            person: Person(
+                name: "Alexey",
+                surname: "Kanaev",
+                profession: "Lawyer",
+                hobby: "Tennis, Sky, Bike"
+            )
+        )
+    }
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let profession: String
+    let hobby: String
+}
