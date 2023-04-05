3e17d24209b88484718db679cde9075db43c5321
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-04-05 20:51:06 +0300

    Logic TabBar 4

---
+++ LoginApp/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  LoginApp
+//
+//  Created by Alexey Kanaev on 4/5/23.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
